20365d3fd622dfbcbca3e46c977d337f1d516812
tomsky <tomsky@email.com>
2024-02-26 15:01:06 +0000

    datakafka project initial

---
+++ dataKafka/src/main/java/com/kafkoid/dataKafka/DataKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kafkoid.dataKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataKafkaApplication.class, args);
+	}
+
+}
---
+++ dataKafka/src/test/java/com/kafkoid/dataKafka/DataKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkoid.dataKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
