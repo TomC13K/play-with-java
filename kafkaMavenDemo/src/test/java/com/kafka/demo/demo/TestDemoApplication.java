54d29c3708f5bb3f91278214af7d01ce9b9d6d4f
Tomas Ciernik <tomsky@email.com>
2024-03-14 16:54:18 +0000

    broken testcontainer test of a topic

--- kafkaMavenDemo/src/test/java/com/kafka/demo/demo/TestDemoApplication.java
+++ kafkaMavenDemo/src/test/java/com/kafka/demo/demo/TestDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 public class TestDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(DemoApplication::main).with(TestDemoApplication.class).run(args);
+		SpringApplication.from(KafkaProducerConsumerApplication::main).with(TestDemoApplication.class).run(args);
 	}
 
 }
