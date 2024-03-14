17818116c8e2ed1d2f784d67a50d186cfc69389e
Tomas Ciernik <tomsky@email.com>
2024-03-14 14:59:04 +0000

    initial project with kafka n maven

---
+++ kafkaMavenDemo/src/test/java/com/kafka/demo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.demo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ kafkaMavenDemo/src/test/java/com/kafka/demo/demo/TestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.demo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(DemoApplication::main).with(TestDemoApplication.class).run(args);
+	}
+
+}
