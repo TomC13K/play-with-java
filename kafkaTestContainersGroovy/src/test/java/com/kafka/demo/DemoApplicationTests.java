2946fd14370f58f8dda9b2b167d45f518dcd0b33
Tomas Ciernik <tomsky@email.com>
2024-03-18 08:39:12 +0000

    files added

---
+++ kafkaTestContainersGroovy/src/test/java/com/kafka/demo/DemoApplicationTests.java
@@ -0,0 +1,19 @@
+package com.kafka.demo;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(TestDataSetup.class)
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertNotNull(DemoApplication.class);
+	}
+
+}
